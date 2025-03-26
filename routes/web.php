faeda3bc7a45ef4922426d08418109605843a137
eTHICalhackerzzz <perater.kyle@gmail.com>
2025-03-26 17:05:14 +0800

    koow
    kowdoiwe

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,4 @@ Route::resource('colleges', CollegeController::class);
 Route::get('/select-college', [CollegeController::class, 'select'])->name('colleges.select');
 
 Route::resource('departments', DepartmentController::class);
-
-
-// yabag //
+\\kugmo
