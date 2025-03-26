d169cd988d30c370ddccf5b6ebd05716f980e167
eTHICalhackerzzz <perater.kyle@gmail.com>
2025-03-26 20:49:21 +0800

    //67094
    //67094

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 //23222
+//67094
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
